1b3150e2fb0edd8486bdd12e73cce3c3cbdf49b5
YoussefLaunchUp <youssef.Ah.zayed@gmail.com>
2024-02-15 16:12:30 -0500

    update Customer move Code

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 extends Node
 @export var customerScene: PackedScene
 var customer
-var positionToBeAt =  Vector2(100,100)
+var positionToBeAt =  Vector2(0,300)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,11 +12,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (customer && customer.positon != positionToBeAt):
-		customer.postion.y += 1 
+	if (customer && customer.position != positionToBeAt):
+		customer.position = Vector2(positionToBeAt.x,customer.position.y + 500 * delta)
+		if (customer.position.y > positionToBeAt.y):
+			customer.position.y = positionToBeAt.y
 
 
 func _on_add_customer_timeout():
+	positionToBeAt.x += 100
+	if (positionToBeAt.x > 900):
+		positionToBeAt.x = 100
 	customer = customerScene.instantiate()
-	customer.position = Vector2(100,0)
+	add_child(customer)
+	customer.position = Vector2(positionToBeAt.x,0)
 	
